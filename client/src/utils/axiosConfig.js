a08f1a86af3a7e7d95600731cb711a341313cf0d
Teja <tejaprom@gmail.com>
2025-07-12 16:14:07 +0530

    favicon added

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -6,71 +6,71 @@ const axiosInstance = axios.create({
 });
 
 // Request interceptor to attach access token
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("accessToken");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("accessToken");
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
 
 // Response interceptor to handle token expiry and retry failed requests
-let isRefreshing = false;
-let failedQueue = [];
+// let isRefreshing = false;
+// let failedQueue = [];
 
-const processQueue = (error, token = null) => {
-  failedQueue.forEach((prom) => {
-    if (error) {
-      prom.reject(error);
-    } else {
-      prom.resolve(token);
-    }
-  });
-  failedQueue = [];
-};
+// const processQueue = (error, token = null) => {
+//   failedQueue.forEach((prom) => {
+//     if (error) {
+//       prom.reject(error);
+//     } else {
+//       prom.resolve(token);
+//     }
+//   });
+//   failedQueue = [];
+// };
 
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
+// axiosInstance.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const originalRequest = error.config;
 
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      if (isRefreshing) {
-        return new Promise((resolve, reject) => {
-          failedQueue.push({ resolve, reject });
-        })
-          .then((token) => {
-            originalRequest.headers.Authorization = `Bearer ${token}`;
-            return axiosInstance(originalRequest);
-          })
-          .catch((err) => Promise.reject(err));
-      }
+//     if (error.response?.status === 401 && !originalRequest._retry) {
+//       if (isRefreshing) {
+//         return new Promise((resolve, reject) => {
+//           failedQueue.push({ resolve, reject });
+//         })
+//           .then((token) => {
+//             originalRequest.headers.Authorization = `Bearer ${token}`;
+//             return axiosInstance(originalRequest);
+//           })
+//           .catch((err) => Promise.reject(err));
+//       }
 
-      originalRequest._retry = true;
-      isRefreshing = true;
+//       originalRequest._retry = true;
+//       isRefreshing = true;
 
-      try {
-        const res = await axiosInstance.post("/auth/refresh-token");
-        const newAccessToken = res.data.accessToken;
-        localStorage.setItem("accessToken", newAccessToken);
-        axiosInstance.defaults.headers.Authorization = `Bearer ${newAccessToken}`;
-        processQueue(null, newAccessToken);
-        return axiosInstance(originalRequest);
-      } catch (err) {
-        processQueue(err, null);
-        localStorage.removeItem("accessToken");
-        window.location.href = "/login";
-        return Promise.reject(err);
-      } finally {
-        isRefreshing = false;
-      }
-    }
+//       try {
+//         const res = await axiosInstance.post("/auth/refresh-token");
+//         const newAccessToken = res.data.accessToken;
+//         localStorage.setItem("accessToken", newAccessToken);
+//         axiosInstance.defaults.headers.Authorization = `Bearer ${newAccessToken}`;
+//         processQueue(null, newAccessToken);
+//         return axiosInstance(originalRequest);
+//       } catch (err) {
+//         processQueue(err, null);
+//         localStorage.removeItem("accessToken");
+//         window.location.href = "/login";
+//         return Promise.reject(err);
+//       } finally {
+//         isRefreshing = false;
+//       }
+//     }
 
-    return Promise.reject(error);
-  }
-);
+//     return Promise.reject(error);
+//   }
+// );
 
 export default axiosInstance;
